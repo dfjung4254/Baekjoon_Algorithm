ce781e6ef5ec2af217e8202fd48f6b1384377281
dfjung4254 <rmsghk4254@naver.com>
2018-12-04 03:48:19 +0900

    commit

--- 4485.cpp
+++ 4485.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-
-    cout << "hello world" << endl;
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+        int tc = 0;
+    while(++tc){
+        int n;
+        cin >> n;
+        if(n==0)
+            break;
+        int matrix[n][n];
+        bool visited[n][n];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                cin >> matrix[i][j];
+                
+            }
+        }
+    }
     return 0;
 }
