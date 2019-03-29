39fa6abe5518a7d5c20e004090c35fa2e257fe62
JKH <rmsghk4254@naver.com>
2019-03-29 22:44:54 +0900

    2019.03.29

--- hw2a.cpp
+++ hw2a.cpp
@@ -30,16 +30,69 @@ double exchangeSort(vector<int> &v){
     return (double)clock() - ck;
 }
 
-void mergeSort(vector<int> &v){
+void merge(vector<int> &v, int low1, int high1, int low2, int high2)
+{
+    int init = low1;
+    int end = high2;
+    vector<int> nv;
+    while (low1 <= high1 && low2 <= high2)
+    {
+        if (v[low1] > v[low2])
+        {
+            nv.push_back(v[low2++]);
+        }
+        else
+        {
+            nv.push_back(v[low1++]);
+        }
+    }
+    while (low1 <= high1)
+    {
+        nv.push_back(v[low1++]);
+    }
+    while (low2 <= high2)
+    {
+        nv.push_back(v[low2++]);
+    }
 
+    int idx = 0;
+    for (int i = init; i <= end; i++)
+    {
+        v[i] = nv[idx++];
+    }
+}
 
+void mergeSort(vector<int> &v, int size, int low, int high){
+
+    if(size == 1){
+        return;
+    }
+    int mid = (low + high) / 2;
+    int n_size = size / 2;
+    mergeSort(v, size - n_size, low, mid);
+    mergeSort(v, n_size, mid + 1, high);
+    merge(v, low, mid, mid + 1, high);
+}
+
+void swap(vector<int> &v, int a, int b){
+
+    int tp = v[a];
+    v[a] = v[b];
+    v[b] = tp;
 
 }
 
-void quickSort(vector<int> &v){
+void quickSort(vector<int> &v, int low, int high){
 
+    int pivot = v[low];
 
-
+    if(low < high){
+        /* 먼저 파티션 한다 */
+        int i, j;
+        for (int i = low + 1; i <= high; i++){
+            
+        }
+    }
 }
 
 int d1, d2, d3;
@@ -62,10 +115,12 @@ int main(){
 
 
     }
-    vector<int> vv;
-    for (int i = 0; i < d1; i++){
-        vv.push_back(rand());
+    vector<int> vv; 
+    for (int i = 10000; i > 0; i--){
+        vv.push_back(i);
     }
+    
+    //mergeSort(vv, vv.size(), 0, vv.size() - 1);
     cout << exchangeSort(vv) << '\n';
     for (int i = 0; i < vv.size(); i++){
         cout << vv[i] << " ";
