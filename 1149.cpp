b8d47be680516e4f228c0ec5da1b9e34a8d8bf17
JKH <rmsghk4254@naver.com>
2019-04-10 16:14:15 +0900

    2019.04.10

--- 1149.cpp
+++ 1149.cpp
@@ -1,57 +1,28 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int n;
-struct House
-{
-    int rgb[3];
-};
-House matrix[1000];
-int colored[1000];
-int ans = 987654321;
+int dp[1001][3];
+int cost[1001][3];
 
-void dp(int colored[], int curColor, int index, int sum){
-
-    if(index >= n){
-        /* 완료 */
-        if(ans > sum){
-            ans = sum;
-        }
-        return;
-    }
-
-    colored[index] = curColor;
-    sum += matrix[index].rgb[curColor];
-
-    int nextC1 = (curColor + 1) % 3;
-    int nextC2 = (curColor + 2) % 3;
-
-    dp(colored, nextC1, index + 1, sum);
-    dp(colored, nextC2, index + 1, sum);
-
-}
-
-int main()
-{
+int main(){
 
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        int _r, _g, _b;
-        cin >> _r >> _g >> _b;
-        matrix[i].rgb[0] = _r;
-        matrix[i].rgb[1] = _g;
-        matrix[i].rgb[2] = _b;
+    for (int i = 1; i <= n; i++){
+        cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
     }
 
-    for (int i = 0; i < 3; i++)
-    {
-        dp(colored, i, 0, 0);
+    for (int i = 1; i <= n; i++){
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
     }
-    cout << ans << '\n';
+
+    cout << min(min(dp[n][0], dp[n][1]), dp[n][2]) << '\n';
 
     return 0;
 }
