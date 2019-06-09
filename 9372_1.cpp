cfbddeb1491d32ecb4fb5afdffcb80048650ef8e
JKH <rmsghk4254@naver.com>
2019-06-09 22:00:51 +0900

    2019.06.09

---
+++ 9372_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+    MST 문제 Prim 으로 풀기
+    N^2 으로 풀기
+
+*/
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    while(true)
+    {
+        int n, m;
+        cin >> n >> m;
+
+    }
+
+    return 0;
+}
