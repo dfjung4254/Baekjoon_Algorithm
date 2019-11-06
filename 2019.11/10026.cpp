1748f430144e68ff1a934065c96e154126badee1
dfjung4254 <rmsghk4254@naver.com>
2019-11-07 03:15:03 +0900

    start PS

--- 2019.11/10026.cpp
+++ 2019.11/10026.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 
 using namespace std;
 
+char matrix[101][101];
+int visited[101][101];
+
+int startBfs(int i, int j, int num){
+
+  visited[i][j] = num;
+  char standard = matrix[i][j];
+  queue<pair<int, int>> q;
+  q.push(make_pair(i, j));
+  
+
+}
+
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+  int n;
+  cin >> n;
 
-    int n;
-    vector<vector<char>> origin;
-    cin >> n;
-
-    for(int i = 0; i < n; i++){
-        vector<char> vv;
-        origin.push_back(vv);
-        for(int j = 0; j < n; j++){
-            char ch;
-            cin >> ch;
-            origin.back().push_back(ch);
-        }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> matrix[i][j];
     }
+  } 
 
-    
+  /* case 1 */
+  int case1 = 0;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      if(visited[i][j] == 0){
+        startBfs(i, j, ++case1);
+      }
+    }
+  }
 
-
-
-    return 0;
+  return 0;
 }
